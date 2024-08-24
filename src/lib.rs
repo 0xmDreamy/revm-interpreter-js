3aa83e204da7930a7b67924e7d8f979e62403c1e
0xmDreamy <0xmDreamy@proton.me>
2024-08-24 17:47:20 +0200

    feat!: Use a typed struct as the interpret parameter

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,14 @@
 
 extern crate alloc;
 
-use alloc::{string::String, vec::Vec};
+use alloc::{boxed::Box, format, string::String, string::ToString, vec::Vec};
 use core::str::FromStr;
+use serde::{Deserialize, Serialize};
+use tsify_next::Tsify;
 
 use revm_interpreter::{
     opcode::make_instruction_table,
-    primitives::{spec_to_generic, Address, Bytecode, Bytes, SpecId, U256},
+    primitives::{spec_to_generic, Bytecode, SpecId, U256},
     Contract, DummyHost, Interpreter, InterpreterAction, SharedMemory,
 };
 use wasm_bindgen::prelude::*;
@@ -35,70 +37,76 @@ impl From<js_sys::BigInt> for BigInt {
     }
 }
 
+#[derive(Tsify, Serialize, Deserialize)]
+#[tsify(into_wasm_abi, from_wasm_abi, large_number_types_as_bigints)]
+#[serde(rename_all = "camelCase")]
+pub struct InterpretParams {
+    #[tsify(type = "Uint8Array")]
+    #[serde(with = "serde_bytes")]
+    /// The bytecode to interpret.
+    bytecode: Vec<u8>,
+    #[tsify(type = "Uint8Array")]
+    #[serde(default, with = "serde_bytes")]
+    /// The data to pass to the contract.
+    data: Vec<u8>,
+    #[tsify(type = "bigint")]
+    #[serde(default, with = "serde_wasm_bindgen::preserve")]
+    /// The value to send to the contract.
+    value: js_sys::BigInt,
+    #[tsify(type = "Uint8Array")]
+    #[serde(default, with = "serde_bytes")]
+    /// The address of the sender. Default is zero address.
+    from: [u8; 20],
+    #[tsify(type = "Uint8Array")]
+    #[serde(default, with = "serde_bytes")]
+    /// The address of the contract. Default is zero address.
+    target_address: [u8; 20],
+    #[tsify(type = "Uint8Array")]
+    #[serde(default, with = "serde_bytes")]
+    /// The address of the bytecode. Default is target address.
+    bytecode_address: Option<[u8; 20]>,
+    #[tsify(type = "bigint")]
+    #[serde(default)]
+    /// The gas limit for interpreter. 0 <= gas_limit <= type(uint64).max. Default type(uint64).max.
+    gas_limit: Option<u64>,
+    #[serde(default)]
+    /// Whether the call is static. Default is false.
+    static_call: bool,
+    #[tsify(
+        type = "'Frontier' | 'Homestead' | 'Tangerine' | 'Spurious' | 'Byzantium' | 'Constantinople' | 'Petersburg' | 'Istanbul' | 'MuirGlacier' | 'Berlin' | 'London' | 'Merge' | 'Shanghai' | 'Cancun' | 'Prague' | 'PragueEOF'"
+    )]
+    #[serde(default)]
+    /// The name of the spec to use. Default is LATEST. See: https://github.com/bluealloy/revm/blob/main/crates/primitives/src/specification.rs#L97.
+    specification_name: Option<String>,
+}
+
 /// Interpret the given bytecode.
 ///
-/// @param {Uint8Array} bytecode - The bytecode to interpret.
-/// @param {Uint8Array} [data] - The data to pass to the contract.
-/// @param {bigint} [value] - The value to send to the contract.
-/// @param {Uint8Array} [from] - The address of the sender. Default is zero address.
-/// @param {Uint8Array} [targetAddress] - The address of the contract. Default is zero address.
-/// @param {Uint8Array} [bytecodeAddress] - The address of the bytecode. Default is target address.
-/// @param {bigint} [gasLimit] - The gas limit for interpreter. 0 <= gas_limit <= type(uint64).max. Default type(uint64).max.
-/// @param {boolean} [staticCall=false] - Whether the call is static. Default is false.
-/// @param {string} [specificationName] - The name of the spec to use. Default is LATEST. See: https://github.com/bluealloy/revm/blob/main/crates/primitives/src/specification.rs#L97.
+/// @param {InterpretParams} params - The parameters interpreter parameters.
 /// @returns {Uint8Array} The result of the interpretation.
 #[allow(non_snake_case)]
 #[wasm_bindgen(skip_jsdoc)]
-pub fn interpret(
-    bytecode: &[u8],
-    data: Option<Vec<u8>>,
-    value: Option<js_sys::BigInt>,
-    from: Option<Vec<u8>>,
-    targetAddress: Option<Vec<u8>>,
-    bytecodeAddress: Option<Vec<u8>>,
-    gasLimit: Option<u64>,
-    staticCall: Option<bool>,
-    specificationName: Option<String>,
-) -> Result<Vec<u8>, js_sys::Error> {
+pub fn interpret(params: InterpretParams) -> Result<Vec<u8>, js_sys::Error> {
     let contract = Contract::new(
-        data.map_or_else(|| Bytes::default(), |v| Bytes::from_iter(v)),
-        Bytecode::new_raw(Bytes::from_iter(bytecode)),
+        params.data.into(),
+        Bytecode::new_raw(params.bytecode.into()),
         None,
-        targetAddress.map_or_else(
-            || Ok(Address::ZERO),
-            |v| {
-                v.as_slice()
-                    .try_into()
-                    .map_err(|_| js_sys::Error::new("Bad target address"))
-            },
-        )?,
-        match bytecodeAddress {
-            Some(v) => Some(
-                v.as_slice()
-                    .try_into()
-                    .map_err(|_| js_sys::Error::new("Bad bytecode address"))?,
-            ),
-            None => None,
-        },
-        from.map_or_else(
-            || Ok(Address::ZERO),
-            |v| {
-                v.as_slice()
-                    .try_into()
-                    .map_err(|_| js_sys::Error::new("Bad from address"))
-            },
-        )?,
-        value.map_or_else(|| Ok(U256::ZERO), |v| BigInt::from(v).try_into())?,
+        params.target_address.into(),
+        params.bytecode_address.map(|v| v.into()),
+        params.from.into(),
+        BigInt::from(params.value).try_into()?,
     );
 
     let mut interpreter = Interpreter::new(
         contract,
-        gasLimit.unwrap_or_else(|| u64::MAX),
-        staticCall.unwrap_or(false),
+        params.gas_limit.unwrap_or_else(|| u64::MAX),
+        params.static_call,
     );
 
     let mut host = DummyHost::default();
-    let spec_id = specificationName.map_or_else(|| SpecId::LATEST, |v| SpecId::from(v.as_str()));
+    let spec_id = params
+        .specification_name
+        .map_or_else(|| SpecId::LATEST, |v| SpecId::from(v.as_str()));
     let table = spec_to_generic!(spec_id, &make_instruction_table::<DummyHost, SPEC>());
 
     if let InterpreterAction::Return { result } =
